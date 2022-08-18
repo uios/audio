a28849ca49be9d1230d82ae163d38e87b5a703ab
thebanon <91961816+thebanon@users.noreply.github.com>
2022-08-18 11:06:30 -0400

    Add Dependency Loader
    Create conditional logic for scripts and styles

--- cdn/js/router.js
+++ cdn/js/router.js
@@ -13,8 +13,11 @@ String.prototype.router = async function(params) {
 
             if (!pop && !["blob:"].includes(window.location.protocol)) {
                 const hash = global.domains.domain === "github" ? "/#" : "";
-                var goto = window.global.domains.subdomain === "uios" ? '/audio' : '';
-                const link = goto + hash + route.path + route.search + route.hash;
+                var goto = window.global.domains.subdomain === "uios" ? '/audio' : '';                
+                const link = hash.length > 0 ? 
+                    goto + hash + (route.hash.length>0 ? route.hash.split('#')[1] : route.path) + route.search:
+                    goto + route.path + route.search + route.hash;
+                console.log({hash,route,link},route.hash.split('#')[1]);
                 history.pushState(link, '', link);
             }
 
